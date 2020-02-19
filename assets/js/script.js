c6fe9e1fe13605939c3c30bd81045704b7c0d029
saniketprdxn <saniketprdxn@github.com>
2020-02-19 17:53:23 +0530

    input field validated

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,21 @@ var changingBackground = document.querySelector('main .wrapper');
 var submit = document.querySelector('.submit');
 var display = document.querySelector('.display');
 var dataRecieved;
+var onlyLocation;
+var currentLocation;
+var cityName;
+var name;
 submit.addEventListener('click',function(e) {
   var city = document.querySelector('.input_text').value;
   var Url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=a1a305c08d22da9f0357ca84f0bd5cec&units=metric";
-
   e.preventDefault();
   fetch(Url)
   .then(function (response) {
     return response.json();
     })
     .then(function (data) {
-      console.log(data);
       dataRecieved = data;
+      validate(city);
       displayData();
     })
     .catch(function (err) {
@@ -22,7 +25,27 @@ submit.addEventListener('click',function(e) {
   });
 });
 
+// validation if empty string or not a correct city name
+function validate(city) {
+  debugger;
+  if(dataRecieved.cod === "404") {
+    alert('Enter correct city name');    
+  } 
+  cityName = dataRecieved.name;
+  name = cityName.toLowerCase();
+  if (name.indexOf(city) === -1 || city === " ") {
+    alert('Enter correct city name');
+    clear();
+  } else {
+    return true;
+  }
+}
+
 function displayData() {
+debugger
+// validate before display 
+  if(display.innerHTML === "" ||   currentLocation !== name) {
+    clear();
 // Creating Element-Childs of .display class element
   var weatherhead = document.createElement('h1');
   var descriptionTitle = document.createElement('p');
@@ -56,39 +79,55 @@ function displayData() {
   var windSpeedval = dataRecieved['wind']['speed'];
 
 // displaying retrieved data to innerText of elements 
-  desc.innerText = "weather status: "+weatherstatus;
+  desc.innerText = "city location: "+cityName;
   temperatureValue.innerText = "temperature Value: "+tempValue+"\xB0 C";
   windSpeedValue.innerText = "Wind speed: "+windSpeedval+"km/hr";
   currentWeather.innerText = weatherstatus;
-  desc.innerText = "weather status: "+weatherstatus;
   iconUnicode = "\\"+iconUnicode;
 
+// checking if it is a previous entered location or not
+  var previousLocation = desc.innerText;
+  var n = 2; 
+  var a = previousLocation.split(' ');
+  onlyLocation =  a.slice(n).join(' ');
+  currentLocation = onlyLocation.toLowerCase();
 // calling changeBgColor to change the background image of [main .wrapper] element
   changeBgColor(weatherstatus);
+  } else {
+    return false;
+  }
 }
 function changeBgColor(weatherstatus) {
-  debugger;
   var icon = dataRecieved.weather[0].icon,
   weatherType;
 // checking whether the weatherType is matching the user inputted location weatherType
   if(icon === "11d"){
     weatherType=  "thunderstorm";
-  }else if(icon === "09d"){
+  }else if(icon === "09d" || icon === "09n"){
     weatherType= "drizzle";
-  }else if(icon === "10d"){
+  }else if(icon === "10d" || icon === "10n"){
     weatherType= "rain";
-  } else if(icon === "13d"){
+  } else if(icon === "13d" || icon === "13n"){
     weatherType= "snow";
   }else if(icon === "01d" || icon === "01n"){
     weatherType= "clear sky";
   }else if(icon === "02d" || icon === "02n" || icon === "03d" || icon === "03n" || icon === "04d" || icon === "04n"){
     weatherType= "cloudy";
+  }else if(icon === "50d" || icon === "50n"){
+    weatherType= "smoke";
   }else {
     weatherType = "default";
   };
 // Changing the background image of [main .wrapper] element
   var urlbgcolor = weatherType+".jpg";
-  changingBackground.style.background = 'url("assets/images/'+urlbgcolor+'") no-repeat';
+  var backgroundUrl = 'url("assets/images/'+urlbgcolor+'") no-repeat';
+  // changingBackground.setAttribute('background',backgroundUrl);
+  // changingBackground.setAttribute('backgroundSize','cover');
+  // changingBackground.setAttribute('backgroundPosition','center');
+  changingBackground.style.background = backgroundUrl;
   changingBackground.style.backgroundSize = 'cover';
   changingBackground.style.backgroundPosition = 'center'; 
 }
+
+// clear the display field 
+function clear() { display.innerHTML = "";}
